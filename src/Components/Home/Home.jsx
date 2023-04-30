09e940ced642fdd752e7cc9f2267acc6de39c87b
husnainkhalid <mhusnainkhalid1122@gmail.com>
2023-04-30 09:35:03 +0500

    final-code

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React,{useEffect} from "react";
 import "./home.scss";
 import video from "../../assets/video.mp4";
 import { GrLocation } from "react-icons/gr";
 import { HiFilter } from "react-icons/hi";
+import { FiFacebook } from "react-icons/fi";
+import { FaTripadvisor } from "react-icons/fa";
+import { AiOutlineInstagram } from "react-icons/ai";
+import { TbApps } from "react-icons/tb";
+import { BsListTask } from "react-icons/bs";
+
+import Aos from 'aos'
+import 'aos/dist/aos.css'
 
 const Home = () => {
+
+//pakage used to scrool animation
+useEffect(() => {
+Aos.init({duration:2000})
+}, [])
+
+
   return (
     <section className="home">
       <div className="overlay"></div>
@@ -19,10 +34,10 @@ const Home = () => {
 
       <div className="home-content container">
         <div className="textDiv">
-          <span className="smallText">Our Package</span>
-          <h1 className="homeTitle">Search Your Holidays</h1>
+          <span data-aos="fade-up"  className="smallText">Our Package</span>
+          <h1 data-aos="fade-up" className="homeTitle">Search Your Holidays</h1>
         </div>
-        <div className="cardDiv">
+        <div data-aos="fade-up" className="cardDiv grid">
           <div className="destinationInput">
             <label htmlFor="city">Search Your Destination</label>
             <div className="input flex">
@@ -51,6 +66,17 @@ const Home = () => {
   <span>More Filters</span>
 </div>
 
+        </div>
+        <div data-aos="fade-up" className="homeFooterIcons flex">
+          <div className="rightIcons">
+            <FiFacebook className="icon"/>
+            <AiOutlineInstagram className="icon"/>
+            <FaTripadvisor className="icon"/>
+          </div>
+          <div className="leftIcons">
+            <BsListTask className="icon"/>
+            <TbApps className="icon"/>
+          </div>
         </div>
       </div>
     </section>
